697871f6a31ca4ab90a4124902bc729c1083984e
pkgit123 <petekim@gmail.com>
2019-02-10 18:02:17 -0800

    added a sample_config.py file to illustrate how to store API token credentials in a secure way in a public repo.

---
+++ sample_config.py
@@ -0,0 +1,10 @@
+# Make sure to add this file to the .gitignore file
+
+# Since this is a sample_config.py, 
+# no need to add to .gitignore file.
+# This file is just an example for teaching purposes.
+# Your actual config.py file should be added to .gitignore, 
+# and therefore will be invisible on the github website (aka remote repo).
+
+# Adding GuruFocus Rest API credentials
+gurufocus_api_token = 'here_is_where_your_api_token_should_go'
